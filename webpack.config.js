94040e5b75c13501ed819427f1f4fbea32851ebc
Nate Gross <nate@outcastgames.com>
2017-04-06 07:58:25 -0700

    Changed lib versions to match lecture and added more error handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,50 @@
-const webpack = require('webpack');
+var webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        'script-loader!jquery/dist/jquery.min.js',
-        'script-loader!foundation-sites/dist/foundation.min.js',
-        './app/app.jsx'
-    ],
-    externals: {
-        jquery: 'jQuery'
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx',
+      openWeatherMap: 'app/api/openWeatherMap.jsx',
+      ErrorModal: 'app/components/ErrorModal.jsx'
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            '$': 'jQuery',
-            'jQuery': 'jquery'
-        })
-    ],
-    output: {
-        path: __dirname,
-        filename: './public/bundle.js'
-    },
-    resolve: {
-        alias: {
-            Main: __dirname + '/app/components/Main.jsx',
-            Nav: __dirname + '/app/components/Nav.jsx',
-            Weather: __dirname + '/app/components/Weather.jsx',
-            About: __dirname + '/app/components/About.jsx',
-            Examples: __dirname + '/app/components/Examples.jsx',
-            WeatherForm: __dirname + '/app/components/WeatherForm.jsx',
-            WeatherMessage: __dirname + '/app/components/WeatherMessage.jsx',
-            openWeatherMap: __dirname + '/app/api/openWeatherMap.jsx',
-            ErrorModal: __dirname + '/app/components/ErrorModal.jsx',
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
         },
-        modules: ['node_modules'],
-        extensions: ['.js', '.jsx']
-        
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    cacheDirectory: true
-                }
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map'
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
